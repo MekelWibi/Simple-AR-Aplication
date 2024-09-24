669982fc7012df48b01a5d150d488a5e3d4a4636
michaelw2002 <michael.wibisono93@gmail.com>
2024-09-24 17:46:35 +0700

    Add and edit feature

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -6,6 +6,7 @@ public class ButtonController : MonoBehaviour
 {
     public GameObject addPanel;
     public GameObject mainPanel;
+    public GameObject editPanel;
 
     public void ShowAddPanel()
     {
@@ -18,4 +19,17 @@ public class ButtonController : MonoBehaviour
         addPanel.SetActive(false);
         mainPanel.SetActive(true);
     }
+
+    public void ShowEditUIPanel()
+    {
+        mainPanel.SetActive(false);
+        editPanel.SetActive(true);
+    }
+
+    public void EdittoMainPanel()
+    {
+        editPanel.SetActive(false);
+        mainPanel.SetActive(true);
+    }
+
 }
